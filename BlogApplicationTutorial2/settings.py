535ab4b1c75b8b9b8009e20155d93aa23ce5a8c9
Wax30d <shaxzodbek_sobitov@mail.ru>
2022-03-08 16:34:09 +0500

    Installed django-crispy-forms, connected to the project and configured crispy template package.

--- BlogApplicationTutorial2/settings.py
+++ BlogApplicationTutorial2/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
